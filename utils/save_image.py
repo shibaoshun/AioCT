9184975a095b83b9dad89c1014b8b2bfa00720a3
shibaoshun <shibaoshun@users.noreply.github.com>
2025-08-19 10:12:01 +0800

    Delete utils directory

--- utils/save_image.py
+++
@@ -1,6 +0,0 @@
-import os
-import matplotlib.pyplot as plt
-def imwrite(idx, dir, datalist):
-    for i in range(len(datalist)):
-        file_dir = dir[i] + str(idx)+'.png'
-        plt.imsave(file_dir, datalist[i].data.cpu().numpy().squeeze(), cmap="gray")
